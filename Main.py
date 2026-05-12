654130ac8f6e98104dc207f9b4935b00bc018901
Gpelmore <Gpelmore0066@gmail.com>
2026-05-11 19:11:17 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,7 @@
-print("Hi")
+class Pet:
+    def __init__(self, name, color, age, animal_type):
+        self.name = name
+        self.color = color
+        self. age = age
+        self.animal_type = animal_type
+    def
